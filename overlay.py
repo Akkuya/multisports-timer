787ae17327a5e3c9848ea41bf74b4622c45b07a5
Rishi <satyajitdatta03@gmail.com>
2026-08-26 22:33:39 -0400

    implemented toggle_pause

--- overlay.py
+++ overlay.py
@@ -1,34 +1,40 @@
 import sys
+from PySide6 import QtCore
+from PySide6.QtWidgets import QKeySequenceEdit
 from PySide6.QtCore import QTimer, Qt
 from PySide6.QtWidgets import QApplication, QLabel, QWidget
 
 from SessionState import SessionState
+from hotkeyManager import GlobalHotkeyManager
 from timer import SessionTimer
-
+from typing import override
 
 
 class Overlay(QWidget):
+
     def __init__(self):
         super().__init__()
+        self.HkManager = GlobalHotkeyManager()
         self.setAttribute(Qt.WidgetAttribute.WA_ShowWithoutActivating)
         self.setWindowFlags(
             Qt.WindowType.FramelessWindowHint
             | Qt.WindowType.WindowStaysOnTopHint
             | Qt.WindowType.Tool
-            | Qt.WindowType.WindowDoesNotAcceptFocus
+            
         )
         self.setFixedSize(300, 80)
         self.label = QLabel("15:00", self)
         self.label.setStyleSheet("color: white; font-size: 60px;")
         # Position in top-right corner of primary screen
-        screen = QApplication.primaryScreen().geometry()
-        self.move(screen.width() - self.width() - 20, 20)
-        
+        self.screen = QApplication.primaryScreen().geometry()
+        self.move(self.screen.width() - self.width() - 20, 20)
         self.session = SessionTimer()
         self.session.start()
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.on_tick)
         self.timer.start(1000)
+        self._register_keybinds()
+        
 
     def on_tick(self):
         self.session.tick()
@@ -36,8 +42,12 @@ class Overlay(QWidget):
         
         if self.session.state == SessionState.FINISHED:    
             self.show_finished_state()
+            
     
     def show_finished_state(self):
-        self.setFixedSize(1920, 1080)
+        self.setFixedSize(self.screen.width(), self.screen.height())
         self.move(0,0)
         self.label.setText("Done")
+    
+    def _register_keybinds(self):
+        self.HkManager.register("enter", "toggle pause", self.session.toggle_pause)   
--- timer.py
+++ timer.py
@@ -4,7 +4,7 @@ from PySide6.QtCore import QTimer
 from SessionState import SessionState
 
 class SessionTimer:
-    def __init__(self, duration_seconds=5):
+    def __init__(self, duration_seconds=15*60):
         self.duration = duration_seconds
         self.remaining = duration_seconds
         self.state = SessionState.IDLE
@@ -16,9 +16,19 @@ class SessionTimer:
     def tick(self):
         if self.state != SessionState.RUNNING: return
         self.remaining -= 1
-        if self.remaining == 0:
+        if self.remaining <= 0:
             self.state = SessionState.FINISHED
+    
+    def toggle_pause(self):
+        if self.state == SessionState.FINISHED: return
+        if self.state == SessionState.IDLE: return
         
+        
+        if self.state == SessionState.RUNNING:
+            self.state = SessionState.PAUSED
+        elif self.state == SessionState.PAUSED:
+            self.state = SessionState.RUNNING
+    
     def formatted(self):
         mins, secs = divmod(max(self.remaining, 0), 60)
         return f"{mins:02d}:{secs:02d}"
